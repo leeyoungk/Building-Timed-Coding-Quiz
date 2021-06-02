709d689b7e4dc8383197b206fe02acb02fbbd671
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-06-02 16:18:36 -0700

    created a funtion to display first question

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 // set element by class
 var timeEl = document.querySelector("#timerSeconds");
 var startQuiz = document.querySelector("#start-button");
-var submitNameEl = document.querySelector("#submitName");
+var enterNameEl = document.querySelector("#enterName");
+var submitButtonEl = document.querySelector("#submitButton");
+var submissionResponseEl = document.querySelector("#response");
+var sectionQuestionsEl = document.querySelector(".question-section");
+
+
 var listOfQuestions = [
     {
-        questions: "Commonly used data types DO NOT include:",
+        question: "Commonly used data types DO NOT include:",
         correctAnswer: "C.Alerts",
         choices: ["A.Strings","B.Booleans","C.Alerts","D.Numbers"]
     },
 
     {
-        questions: "The condition in an if/else statment is enclosed within___.",
+        question: "The condition in an if/else statment is enclosed within___.",
         correctAnswer: "C.Parentheses",
         choices: ["A.Quotes","B.Curly brackets","C.Parentheses","D.Square brackets"]
     },
     {
-        questions: "Arrays in JavaScript can be used to store___.",
+        question: "Arrays in JavaScript can be used to store___.",
         correctAnswer: "D.All of the above",
         choices: ["A.Numbers and strings","B.Other arrays","C.Booleans","D.All of the above"]
     },
     {
-        questions: "String values must be enclosed within___when being assigned to variables",
+        question: "String values must be enclosed within___when being assigned to variables",
         correctAnswer: "C.Quotes",
         choices: ["A.Comma","B.Curly brackets","C.Quotes","D.Parentheses"]
     }
@@ -32,7 +37,7 @@ var listOfQuestions = [
 
 // Set time left = 75 seconds 
 var secondsLeft = 75;
-var index = 0;
+// var index = 0;
 
 
 // created a function to run variable secondLeft
@@ -54,15 +59,48 @@ function timeLeft() {
 }
 // created an eventlistener to start the game when button is clicked.
 startQuiz.addEventListener("click", function() {
+    var questionContainer = listOfQuestions[index].question;
+    sectionQuestionsEl.textContent = questionContainer;
+    var choicesContainer = listOfQuestions[index].choices;
     
+    for (let index = 0; index < choicesContainer.length; index++) {
+       var choiceButton = document.createElement("button");
+       choiceButton.textContent = choicesContainer[index];
+       choiceButton.setAttribute("id",index);
+       choiceButton.setAttribute("class", "choice");
+       choiceButton.onclick = answerClick;
+       var listChoice = document.createElement("li");
+       listChoice.appendChild(choiceButton);
+       
+       sectionQuestionsEl.appendChild(listChoice);
+       
+
+    }
     timeLeft();
+    function answerClick (event) {
+        
+        console.log(event.target.innerHTML);
+        if (event.target.innerHTML === listOfQuestions[0].correctAnswer) {
+            console.log("correct");
+
+        }else {
+            console.log("wrong");
+        }
+        
+
+    }
+   
+    
     
 
 });
+
 function submitInitials (event) {
     event.preventDefault();
     console.log(event);
+    var response = " thankyou" + enterNameEl;
+    submissionResponseEl.textcontent = response;
 
 }
-submitNameEl.addEventListener("click",submitInitials);
+submitButtonEl.addEventListener("click",submitInitials);
 
